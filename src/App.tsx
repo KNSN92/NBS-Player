0d226d76bf68794f01be37c83f076f35664c015b
KNSN92 <kekenshin92@gmail.com>
2026-04-28 21:41:42 +0900

    add: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex items-center justify-center bg-white size-full text-8xl text-black">
+      NBS Player!!
+    </main>
+  );
+}
+
+export default App;

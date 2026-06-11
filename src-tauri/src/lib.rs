812febf108a9183678efde80063cdcc27f776df1
KNSN92 <kekenshin92@gmail.com>
2026-06-11 16:08:22 +0900

    update: setupを追加

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,8 @@
+use std::error::Error;
+
+use anyhow::Result;
+use tauri::App;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -8,6 +13,12 @@ pub fn run() {
         )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![])
+        .setup(setup)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn setup(app: &mut App) -> Result<(), Box<dyn Error>> {
+    setup_nbs(app)?;
+    Ok(())
 }
